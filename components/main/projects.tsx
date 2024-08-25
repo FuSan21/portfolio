06c19cd1c28951bd147889a87c9873284506eb8b
FuSan21 <35169687+FuSan21@users.noreply.github.com>
2024-08-26 00:30:35 +0600

    chore: optimized hyperlink to anchor

--- components/main/projects.tsx
+++ components/main/projects.tsx
@@ -3,11 +3,11 @@ import { PROJECTS } from "@/constants";
 
 export const Projects = () => {
   return (
-    <section
-      id="projects"
-      className="flex flex-col items-center justify-center py-20"
-    >
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+    <section className="flex flex-col items-center justify-center py-20">
+      <h1
+        className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20"
+        id="projects"
+      >
         My Projects
       </h1>
       <div className="h-full w-full flex-col md:grid md:grid-cols-3 gap-10 px-10">
