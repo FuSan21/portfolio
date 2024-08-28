3ec257f218334ec2d95c2e6b756b5baab8887376
FuSan21 <35169687+FuSan21@users.noreply.github.com>
2024-08-28 19:41:09 +0600

    feat: Add framer-motion animation to project cards

--- components/main/projects.tsx
+++ components/main/projects.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import { ProjectCard } from "@/components/sub/project-card";
 import { PROJECTS } from "@/constants";
+import { motion } from "framer-motion";
+import { staggerContainer } from "@/lib/motion";
 
 export const Projects = () => {
   return (
-    <section className="flex flex-col items-center justify-center py-20">
+    <motion.section
+      variants={staggerContainer()}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true, amount: 0.25 }}
+      className={"flex flex-col items-center justify-center py-20"}
+    >
       <h1
         className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20"
         id="projects"
@@ -11,8 +21,9 @@ export const Projects = () => {
         My Projects
       </h1>
       <div className="h-full w-full flex-col md:grid md:grid-cols-3 gap-10 px-10">
-        {PROJECTS.map((project) => (
+        {PROJECTS.map((project, i) => (
           <ProjectCard
+            index={i}
             key={project.title}
             src={project.image}
             title={project.title}
@@ -22,6 +33,6 @@ export const Projects = () => {
           />
         ))}
       </div>
-    </section>
+    </motion.section>
   );
 };
--- components/sub/project-card.tsx
+++ components/sub/project-card.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/lib/motion";
 
 type ProjectCardProps = {
+  index: number;
   src: string;
   title: string;
   description: string;
@@ -10,6 +13,7 @@ type ProjectCardProps = {
 };
 
 export const ProjectCard = ({
+  index,
   src,
   title,
   description,
@@ -17,7 +21,8 @@ export const ProjectCard = ({
   source,
 }: ProjectCardProps) => {
   return (
-    <div
+    <motion.div
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
       className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]"
     >
       <Image
@@ -32,24 +37,24 @@ export const ProjectCard = ({
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
         <div className="flex space-x-4">
-            <a
-              href={demo}
-              target="_blank"
-              rel="noreferrer noopener"
-              className="flex items-center p-2.5 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
-            >
-              <span>{"Demo"}</span>
-            </a>
-            <a
-              href={source}
-              target="_blank"
-              rel="noreferrer noopener"
-              className="flex items-center p-2.5 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
-            >
-              <span>{"Source"}</span>
-            </a>
+          <a
+            href={demo}
+            target="_blank"
+            rel="noreferrer noopener"
+            className="flex items-center p-2.5 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+          >
+            <span>{"Demo"}</span>
+          </a>
+          <a
+            href={source}
+            target="_blank"
+            rel="noreferrer noopener"
+            className="flex items-center p-2.5 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+          >
+            <span>{"Source"}</span>
+          </a>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
