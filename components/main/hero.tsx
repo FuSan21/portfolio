0009151a6f3875438d082e4b05ae750c2076abf9
FuSan21 <35169687+FuSan21@users.noreply.github.com>
2024-08-28 23:06:59 +0600

    chore: decreased blackhole image opacity for increasing text visibility

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -54,7 +54,7 @@ export const Hero = () => {
           autoPlay
           muted
           loop
-          className="rotate-180 absolute top-[-340px] left-0 w-full h-full object-cover -z-20"
+          className="rotate-180 absolute top-[-340px] left-0 w-full h-full object-cover -z-20 opacity-[0.7]"
         >
           <source src="/videos/blackhole.webm" type="video/webm" />
         </video>
