c4879cf10c30f968a6f052b87d5c6d589fd77349
FuSan21 <35169687+FuSan21@users.noreply.github.com>
2024-09-05 02:08:38 +0600

    fix: Prevent pagination buttons from scrolling out of view Addressed an issue where projects would scroll out of view when fewer rows were present on a subsequent page. Resolved this by implementing anchor links to ensure the view remains on the projects when a pagination button is clicked.

--- components/main/projects.tsx
+++ components/main/projects.tsx
@@ -1,16 +1,35 @@
 "use client";
 
+import { useState } from "react";
 import { ProjectCard } from "@/components/sub/project-card";
 import { PROJECTS } from "@/constants";
 import { motion } from "framer-motion";
 import { staggerContainer } from "@/lib/motion";
 
 export const Projects = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const projectsPerPage = 6;
+  const totalPages = Math.ceil(PROJECTS.length / projectsPerPage);
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages - 1));
+    window.open("#projects", "_self");
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
+    window.open("#projects", "_self");
+  };
+
+  const startIndex = currentPage * projectsPerPage;
+  const endIndex = startIndex + projectsPerPage;
+  const currentProjects = PROJECTS.slice(startIndex, endIndex);
+
   return (
     <motion.section
-      variants={staggerContainer()}
+      variants={staggerContainer(0.1, 0.2)}
       initial="hidden"
-      whileInView="show"
+      animate="show"
       viewport={{ once: true, amount: 0.2 }}
       className={"flex flex-col items-center justify-center py-20"}
     >
@@ -21,7 +40,7 @@ export const Projects = () => {
         My Projects
       </h1>
       <div className="h-full w-full flex-col md:grid md:grid-cols-3 gap-10 px-10">
-        {PROJECTS.map((project, i) => (
+        {currentProjects.map((project, i) => (
           <ProjectCard
             index={i}
             key={project.title}
@@ -33,6 +52,24 @@ export const Projects = () => {
           />
         ))}
       </div>
+      {totalPages > 1 && (
+        <div className="flex justify-center mt-10">
+          <button
+            className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md mr-2"
+            onClick={handlePrevPage}
+            disabled={currentPage === 0}
+          >
+            Previous
+          </button>
+          <button
+            className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md"
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages - 1}
+          >
+            Next
+          </button>
+        </div>
+      )}
     </motion.section>
   );
 };
